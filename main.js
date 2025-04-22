dfd068f395840747df3dcfb233b73d29003426b4
HeidelV <valerioheidelberg20@gmail.com>
2025-04-22 11:58:08 +0800

    Added a bomb that decreases score.

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
 const gameArea = document.getElementById('gameArea');
 const target = document.getElementById('target');
+const bomb = document.getElementById('bomb');
 const scoreBoard = document.getElementById('scoreBoard');
 
 let score = 0;
+
+function bombTarget() {
+  const gameAreaRect = gameArea.getBoundingClientRect();
+  const maxX = gameAreaRect.width - bomb.offsetWidth;
+  const maxY = gameAreaRect.height - bomb.offsetHeight;
+
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
+
+  bomb.style.left = `${randomX}px`;
+  bomb.style.top = `${randomY}px`;
+}
 
 function moveTarget() {
   const gameAreaRect = gameArea.getBoundingClientRect();
@@ -20,8 +33,14 @@ function increaseScore() {
   score ++;
   scoreBoard.innerHTML = "Score: " + score;
 }
+
+function decreaseScore() {
+  score --;
+  scoreBoard.innerHTML = "Score: " + score;
+}
 // Initial target position
 moveTarget();
+bombTarget();
 
 const ball_listener = target.addEventListener("contextmenu", (e)=> {
   e.preventDefault();
@@ -29,3 +48,12 @@ const ball_listener = target.addEventListener("contextmenu", (e)=> {
   increaseScore();
 })
 
+const bomb_listener = bomb.addEventListener("contextmenu", (e)=> {
+  e.preventDefault();
+  bombTarget();
+  decreaseScore();
+})
+
+setInterval(() => {
+  bombTarget();
+}, 750);
