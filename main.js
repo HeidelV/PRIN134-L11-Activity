70bc63f08d21ad7ed79335fcbd860444f552a15a
HeidelV <valerioheidelberg20@gmail.com>
2025-04-22 11:41:52 +0800

    Added score function and a ball listener.

--- main.js
+++ main.js
@@ -16,5 +16,16 @@ function moveTarget() {
   target.style.top = `${randomY}px`;
 }
 
+function increaseScore() {
+  score ++;
+  scoreBoard.innerHTML = "Score: " + score;
+}
 // Initial target position
 moveTarget();
+
+const ball_listener = target.addEventListener("contextmenu", (e)=> {
+  e.preventDefault();
+  moveTarget();
+  increaseScore();
+})
+
